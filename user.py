3fd24795ec496e989240a0b128b8c868d32ac3ec
comfort museo <you@example.com>
2020-10-01 19:25:28 +0300

    add user class

---
+++ user.py
@@ -0,0 +1,12 @@
+   class User:
+    """
+    Class that generates a new user.
+    """
+
+    def __init__(self,user_name,password):
+
+      
+
+        self.user_name = username
+        self.password= password
+        
