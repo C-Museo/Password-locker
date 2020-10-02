9b2e896df7fdfeec6a9eb94852243c955a4149b7
comfort museo <you@example.com>
2020-10-02 17:39:42 +0300

    add Credentials class

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
    class User:
-    """
-    Class that generates a new user.
-    """
+      '''
+      Class that generates a new user.
+      '''
     User = []
 
     def __init__(self,user_name,password):
@@ -10,8 +10,16 @@
         self.password= password
         
     def save_user(self):
-		'''
-		Function to save a newly created user instance.
-		'''
+		  '''
+		  Function to save a newly created user instance.
+		  '''
 		User.users_list.append(self)
 		
+
+    class Credentials:
+      '''
+      Class to create  account credentials, generate passwords and save their information
+      '''
+    Credentials = []
+
+    def check_user(cls, user_name, password)
